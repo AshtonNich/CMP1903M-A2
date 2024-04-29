f6733c0b2cfb41abc1354d3036c89da999020bf9
AshtonNich <ashtonnicholls3353@gmail.com>
2024-04-29 10:47:01 +0100

    more functionality added to sevensout.cs with options to exit the game, and currently is storing the amount of times rolled and the score system half implemented

--- CMP1903M-A2/SevensOut.cs
+++ CMP1903M-A2/SevensOut.cs
@@ -11,6 +11,9 @@ namespace CMP1903M_A2
     {
         private Die dice1 = new Die();
         private Die dice2 = new Die();
+        private int total;
+        private int temp;
+        private int rolled;
 
         public void Sevens()
         {
@@ -18,18 +21,40 @@ namespace CMP1903M_A2
             string cpuORplayer = Console.ReadLine();
             if (cpuORplayer == "computer") 
             {
-                int roll1 = dice1.Roll();
-                int roll2 = dice2.Roll();
-
-                Console.WriteLine("Press any button to roll");
-                Console.ReadLine();
-                if (roll1 + roll2 == 7)
+                while (true)
                 {
-                    Console.WriteLine("both roll 1 " + roll1 + " and roll 2 " + roll2 + " add up to seven.\nEnding Game!");
-                }
-                else if (roll1 == roll2)
-                {
+                    int roll1 = dice1.Roll();
+                    int roll2 = dice2.Roll();
 
+                    Console.WriteLine("Press enter to roll or type stop to break out of the game");
+                    string Uplay = Console.ReadLine();
+                    if (Uplay == "stop")
+                    {
+                        break;
+                    }
+                    while (Uplay != "stop")
+                    {
+                        if (roll1 + roll2 == 7)
+                        {
+                            Console.WriteLine("both roll one " + roll1 + " and roll two " + roll2 + " add up to seven.\nEnding Game!");
+                            rolled++;
+                            break;
+                        }
+                        else if (roll1 == roll2)
+                        {
+                            Console.WriteLine("both roll one " + roll1 + " and roll two " + roll2 + " are the same! adding it to your total");
+                            temp = roll1 + roll2;
+                            total += temp * 2;
+                            rolled++;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("roll 1 is " + roll1 + " and roll 2 is " + roll2);
+                            break;
+                        }
+
+                    }
                 }
             }
         }
